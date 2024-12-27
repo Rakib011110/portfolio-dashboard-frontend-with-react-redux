fcc8fcabc318105f0806c95fd4d0a46c32dd26b6
Rakibul Islam <rakib011110@gmail.com>
2024-12-27 23:16:44 +0600

    dashboard added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <button className="btn">Button</button>
-      <button className="">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </div>
-  );
+  return <div>WELOME TO MY PORTFOLIO</div>;
 };
 
 export default Home;
