38feab2152fd34280872c160b8bfbf1fd506ffa7
Rakibul Islam <rakib011110@gmail.com>
2025-01-15 12:31:36 +0600

    code update

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,8 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "http://localhost:5000/api",  //
-    baseUrl: "https://persional-web-backend.vercel.app/api",
+    baseUrl: "http://localhost:5000/api",
   }),
   tagTypes: ["Blog", "Projects"],
   endpoints: () => ({}),
