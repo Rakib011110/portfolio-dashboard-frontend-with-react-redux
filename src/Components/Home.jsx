899f43aedc9d840159d1ba562d3998289a615347
Rakibul Islam <rakib011110@gmail.com>
2025-01-15 13:13:12 +0600

    redirect added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 
   return (
     <div className="container mx-auto py-6 px-4">
-      <h1 className="text-2xl font-bold mb-6">All Blogs</h1>
+      <h1 className="text-xl font-bold mb-6">All Blogs</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {blogs?.map((blog) => (
           <div
